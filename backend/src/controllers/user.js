ff34390e27f8a1761f3321066b2535dd43e6c626
Gururaj <kurbetgururaj@gmail.com>
2025-11-16 20:11:36 +0530

    Little Changes

--- backend/src/controllers/user.js
+++ backend/src/controllers/user.js
@@ -325,10 +325,6 @@ const deleteUser = async (req, res) => {
 
 const getAllUsers = async (req, res) => {
   try {
-<<<<<<< HEAD
-=======
-
->>>>>>> 3deba032f2a92f218099de19efac92a984ca6256
     const user = await User.findById(req.user?.userId);
     if (!user || user.role !== "admin") {
       return res.status(401).json({ message: "Unauthorized" });
@@ -342,7 +338,6 @@ const getAllUsers = async (req, res) => {
 };
 
 export {
-<<<<<<< HEAD
   registerUser,
   verifyEmail,
   loginUser,
@@ -356,18 +351,3 @@ export {
   deleteUser,
   getAllUsers,
 };
-=======
-    registerUser,
-    verifyEmail,
-    loginUser,
-    logout,
-    forgetPassword,
-    resetPassword,
-    changePassword,
-    getProfile,
-    uploadPortfolio,
-    updatePhoto,
-    deleteUser,
-    getAllUsers,
-}
->>>>>>> 3deba032f2a92f218099de19efac92a984ca6256
